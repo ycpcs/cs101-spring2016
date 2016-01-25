8c838585f132610bfb4998ee624d958ecec04cf9
David Hovemeyer <david.hovemeyer@gmail.com>
2016-01-25 10:36:27 -0500

    don't include old fix, ensure new fix is applied

--- labs/fixzip.pl
+++ labs/fixzip.pl
@@ -12,7 +12,7 @@ my $fname = shift @ARGV || die;
 ($fname =~ m,^(CS101_.*)\.zip$,) || die;
 my $labname = $1;
 
-Run("unzip $fname");
+Run("unzip $fname > /dev/null");
 
 my $makefile = `find $labname -name Makefile -print`;
 chomp $makefile;
@@ -25,6 +25,8 @@ STDOUT->flush();
 my $ifh = new FileHandle("<$makefile");
 my $ofh = new FileHandle(">$makefile.tmp");
 
+my $fixed = 0;
+
 while (<$ifh>) {
 	if (/^\s*PROJECT_NUMBER\s*:?=/) {
 		print $ofh $_;
@@ -36,8 +38,15 @@ while (<$ifh>) {
 # Make sure it is unset when we invoke g++.\r
 CXX = unset GCC_EXEC_PREFIX && g++\r
 EOF
+		$fixed = 1;
 	} elsif (/^\s*CXX\s+:?=/) {
 		print "Warning: not echoing assignment to CXX macro in $makefile\n";
+	} elsif (/^# Some unknown software/
+		|| /^# sets the GCC_EXEC_PREFIX/
+		|| /^# which causes the cygwin gcc/
+		|| /^GCC_EXEC_PREFIX=/) {
+		# This was the original attempt to fix the issue,
+		# which didn't work.
 	} else {
 		print $ofh $_;
 	}
@@ -46,6 +55,19 @@ $ifh->close();
 $ofh->close();
 Run("mv $makefile.tmp $makefile");
 print "done\n";
+
+if (!$fixed) {
+	print << "EOF2";
+#################################################################
+                             FAIL!!!!
+
+I didn't see any line defining the PROJECT_NUMBER macro,
+so I didn't generate the fix code.  Please edit the makefile
+by hand.  Sorry!
+#################################################################
+EOF2
+	exit 1;
+}
 
 # If zipfile was generated with an extra level, fix it.
 if ($makefile =~ m,^$labname/$labname,) {
@@ -63,5 +85,6 @@ print "Generating new zipfile...";
 STDOUT->flush();
 
 Run("rm -f $fname");
-Run("zip -9r $fname $labname");
+Run("zip -9r $fname $labname > /dev/null");
+Run("rm -rf $labname");
 print "done\n";
